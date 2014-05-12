e7ae16cd17cfafc3317d18da4046a19fe0ab5abf
Ryota ONISHI <ryota.onishi@gmail.com>
2014-05-12 19:35:14 +0900

    grepの正規表現修正と色調整

--- lib/git.zsh
+++ lib/git.zsh
@@ -12,9 +12,9 @@ DIRTY_MARK='%F{33}'$'\u2602''%f'
 
 UNTRACKED_MARK='%F{226}✭%f'
 ADDED_MARK='%F{10}✚%f'
-MODIFIED_MARK='%F{4}✹%f'
-RENAMED_MARK='➜'
-DELETED_MARK='%F{1}✖%f'
+MODIFIED_MARK='%F{165}✹%f'
+RENAMED_MARK='%F{2}➜%f'
+DELETED_MARK='%F{166}✖%f'
 
 # repository check
 function repository_check() {
@@ -54,20 +54,22 @@ function get_status() {
   if $(echo "${STATUS}" | grep -E '^\?\? ' &> /dev/null); then
     MESSAGE="${UNTRACKED_MARK}${MESSAGE}"
   fi
-  if $(echo "${STATUS}" | grep '^(A|M)  ' &> /dev/null); then
+  if $(echo "${STATUS}" | grep '^\(A\|M\) ' &> /dev/null); then
     MESSAGE="${ADDED_MARK}${MESSAGE}"
   fi
-  if $(echo "${STATUS}" | grep '^( M|AM| T) ' &> /dev/null); then
+  if $(echo "${STATUS}" | grep '^\( M\|AM\| T\) ' &> /dev/null); then
     MESSAGE="${MODIFIED_MARK}${MESSAGE}"
   fi
-  if $(echo "${STATUS}" | grep '^R  ' &> /dev/null); then
+  if $(echo "${STATUS}" | grep '^R ' &> /dev/null); then
     MESSAGE="${RENAMED_MARK}${MESSAGE}"
   fi
-  if $(echo "${STATUS}" | grep '^( D|D |AD) ' &> /dev/null); then
+  if $(echo "${STATUS}" | grep '^\( D\|D\|AD\) ' &> /dev/null); then
     MESSAGE="${DELETED_MARK}${MESSAGE}"
   fi
+  echo $MESSAGE
   if [[ -n ${MESSAGE} ]]; then
-    MESSAGE="%F{15}[%f${MESSAGE}%F{15}]%f"
+    TXT_COLOR=$(get_branch_txt_color ${MESSAGE})
+    MESSAGE="%F{${TXT_COLOR}}[%f${MESSAGE}%F{${TXT_COLOR}}]%f"
   fi
   echo $MESSAGE
 }
@@ -78,13 +80,13 @@ function get_branch_bg_color() {
     if $(echo "$1" | grep "${UNTRACKED_MARK}" &> /dev/null); then
       echo 1;
     elif $(echo "$1" | grep "${ADDED_MARK}" &> /dev/null); then
-      echo 1;
+      echo 220;
     elif $(echo "$1" | grep "${MODIFIED_MARK}" &> /dev/null); then
-      echo 226;
+      echo 220;
     elif $(echo "$1" | grep "${RENAMED_MARK}" &> /dev/null); then
-      echo 226;
+      echo 220;
     elif $(echo "$1" | grep "${DELETED_MARK}" &> /dev/null); then
-      echo 1;
+      echo 220;
     else
       echo 2;
     fi
@@ -97,15 +99,15 @@ function get_branch_bg_color() {
 function get_branch_txt_color() {
   if [ $# -eq 1 ]; then
     if $(echo "$1" | grep "${UNTRACKED_MARK}" &> /dev/null); then
-      echo 15;
+      echo 226;
     elif $(echo "$1" | grep "${ADDED_MARK}" &> /dev/null); then
-      echo 15;
+      echo 160;
     elif $(echo "$1" | grep "${MODIFIED_MARK}" &> /dev/null); then
-      echo 15;
+      echo 160;
     elif $(echo "$1" | grep "${RENAMED_MARK}" &> /dev/null); then
-      echo 15;
+      echo 160;
     elif $(echo "$1" | grep "${DELETED_MARK}" &> /dev/null); then
-      echo 15;
+      echo 160;
     else
       echo 15;
     fi
